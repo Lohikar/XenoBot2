f2cd6551ff08c5671429bf8aab87fcc06e3eed8e
Lohikar <lohikar@protonmail.com>
2016-11-22 10:28:47 -0600

    Add documentation to flags

--- XenoBot2.Shared/Flags.cs
+++ XenoBot2.Shared/Flags.cs
@@ -5,10 +5,25 @@ namespace XenoBot2.Shared
 	[Flags]
 	public enum PermissionFlag
 	{
+		/// <summary>
+		///		No permissions required.
+		/// </summary>
 		None = 0,
+		/// <summary>
+		///		Invoker must be able to speak in the affected channel.
+		/// </summary>
 		User = 1,
+		/// <summary>
+		///		Invoker must be a moderator for the affected channel.
+		/// </summary>
 		Moderator = 2,
+		/// <summary>
+		///		Invoker must be an administrator for the server of the affected channel.
+		/// </summary>
 		Administrator = 4,
+		/// <summary>
+		///		Invoker must be the bot administrator.
+		/// </summary>
 		BotAdministrator = 8
 	}
 
@@ -16,10 +31,25 @@ namespace XenoBot2.Shared
 	public enum CommandFlag
 	{
 		None = 0,
+		/// <summary>
+		///		Command is disallowed in normal channels.
+		/// </summary>
 		NoPublicChannel = 1,
+		/// <summary>
+		///		Command is disallowed in private messaging.
+		/// </summary>
 		NoPrivateChannel = 2,
+		/// <summary>
+		///		Command can be used while user is ignored by bot.
+		/// </summary>
 		UsableWhileIgnored = 4,
+		/// <summary>
+		///		Command cannot be disabled.
+		/// </summary>
 		NonDisableable = 8,
-		Hidden = 16,
+		/// <summary>
+		///		Command does not show in help index pages.
+		/// </summary>
+		Hidden = 16
 	}
 }
